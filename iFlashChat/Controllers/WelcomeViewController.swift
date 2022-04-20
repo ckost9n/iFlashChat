d9e6f87c594d453a634f6d5b8a9a796f2c0e8e7c
ckost9n <ckost9n@gmail.com>
2022-04-20 21:31:58 +0300

    first ui setup

--- iFlashChat/Controllers/WelcomeViewController.swift
+++ iFlashChat/Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  iFlashChat
 //
 //  Created by Konstantin on 20.04.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
